62f6b73596d2ef293802b73dfb66c96f5a1f6d43
Zash60 <xath06@gmail.com>
2025-12-31 22:34:57 +0000

    Fix: Correção de depreciação onBackPressed()
    - Substitui método deprecated onBackPressed() por onBackPressedDispatcher
    - Mantém funcionalidade de navegação idêntica
    - Atualiza para API Android moderna

--- app/src/main/java/com/livestream/youtube/SettingsActivity.kt
+++ app/src/main/java/com/livestream/youtube/SettingsActivity.kt
@@ -368,7 +368,7 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
+        onBackPressedDispatcher.onBackPressed()
         return true
     }
 }
